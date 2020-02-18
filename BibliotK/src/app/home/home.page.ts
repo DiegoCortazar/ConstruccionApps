e15e6450a70e5eefd8bb470db31d42dacd7b816f
camiloXweird <49212380+camiloXweird@users.noreply.github.com>
2020-02-17 21:25:50 -0500

    First view to local storage

--- BibliotK/src/app/home/home.page.ts
+++ BibliotK/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +9,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // user: any;
+  // codeV: any;
+  // passwordV: any;
 
-  constructor() {}
-    onLogin(){
-      console.log("ME ESPICHARON");
-    }
+
+  constructor(private storage: Storage, public alertController: AlertController) { }
+  
+  //   async presentAlert(user: any) {
+  //   const alert = await this.alertController.create({
+  //     header: 'Usuario',
+  //     subHeader: 'Datos de Usuario',
+  //     message: 'El usuario: ' +   user.code + ", password: " + user.password, 
+  //     buttons: ['Cerrar', 'Guardar']
+  //   });
+
+  //   await alert.present();
+  // }
+  
+  onLogin() {
+    console.log("ME ESPICHARON");
+    // this.guardar();
+    // this.leer();
+  }
+
+  // guardar() {
+  //   this.storage.set('user', { code: this.codeV, password: this.passwordV });
+  // }
+  // leer() {
+  //   this.storage.get('user').then((respuesta) => {
+  //       console.log(respuesta);
+  //       this.user = respuesta;
+  //       this.presentAlert(respuesta);
+  //   }).catch((error) => {
+  //           console.log(error);
+  //   });
+  // }
+
+  
 }
